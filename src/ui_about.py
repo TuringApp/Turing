8fe62aa32bee73f508d78f473049e803989d4121
zdimension <zippedfire@free.fr>
2018-03-15 16:45:13 +0100

    textbrowser

--- src/ui_about.py
+++ src/ui_about.py
@@ -31,10 +31,11 @@ class Ui_AboutWindow(object):
         self.tab.setObjectName("tab")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.tab)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.textEdit_about = QtWidgets.QTextEdit(self.tab)
-        self.textEdit_about.setReadOnly(True)
-        self.textEdit_about.setObjectName("textEdit_about")
-        self.verticalLayout_2.addWidget(self.textEdit_about)
+        self.textBrowser_about = QtWidgets.QTextBrowser(self.tab)
+        self.textBrowser_about.setReadOnly(True)
+        self.textBrowser_about.setOpenExternalLinks(True)
+        self.textBrowser_about.setObjectName("textBrowser_about")
+        self.verticalLayout_2.addWidget(self.textBrowser_about)
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
@@ -62,7 +63,7 @@ class Ui_AboutWindow(object):
         _translate = QtCore.QCoreApplication.translate
         AboutWindow.setWindowTitle(_translate("AboutWindow", "À propos de Turing"))
         self.label.setText(_translate("AboutWindow", "<img src=\"media/icon_96.png\"/>"))
-        self.textEdit_about.setHtml(_translate("AboutWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.textBrowser_about.setHtml(_translate("AboutWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
