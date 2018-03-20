3714e23acf365e1f5e8d2571a993824548ab25bf
zdimension <zippedfire@free.fr>
2018-03-20 20:43:22 +0100

    start work on algorithm

---
+++ src/algo/stmts/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .AssignStmt import *
+from .BaseStmt import *
+from .BlockStmt import *
+from .DisplayStmt import *
+from .ForStmt import *
+from .FuncStmt import *
+from .IfStmt import *
+from .InputStmt import *
+from .ReturnStmt import *
+from .WhileStmt import *
