0b37885180b9b572679490af7e6cf6b02cbce3d7
zdimension <zippedfire@free.fr>
2018-03-20 23:58:52 +0100

    enhance mutability

--- src/algo/stmts/BlockStmt.py
+++ src/algo/stmts/BlockStmt.py
@@ -9,9 +9,12 @@ class BlockStmt(BaseStmt):
 
     def __init__(self, children: CodeBlock):
         super().__init__()
-        self.children = children
-        for c in self.children:
-            c.parent = self
+        self.set_children(children)
 
     def __str__(self):
         return "{ %s }" % ", ".join(str(x) for x in self.children)
+
+    def set_children(self, children: CodeBlock):
+        self.children = children
+        for c in self.children:
+            c.parent = self
