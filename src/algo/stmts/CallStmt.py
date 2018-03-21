e14703b6bc7e57c43cae45847f33ba567b078e2d
zdimension <zippedfire@free.fr>
2018-03-21 03:15:47 +0100

    add direct call statement

---
+++ src/algo/stmts/CallStmt.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from maths.nodes import AstNode
+from .BaseStmt import *
+
+
+class CallStmt(BaseStmt):
+    function = None
+    arguments = None
+
+    def __init__(self, function: AstNode, arguments: List[AstNode] = []):
+        super().__init__()
+        self.function = function
+        self.arguments = arguments
+
+    def __str__(self):
+        return "[Call %s (%s)]" % (self.function, ", ".join(str(x) for x in self.arguments))
