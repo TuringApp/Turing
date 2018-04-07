67998074e7e960dd0bc5b367aeb1d0f98a54e9a4
zdimension <zippedfire@free.fr>
2018-04-07 16:26:46 +0200

    add comment code

---
+++ src/forms/alg_comment.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+from forms.inline_code_dialog import InlineCodeDialog
+from forms.ui_alg_comment import Ui_AlgoCommentStmt
+from util.widgets import center_widget
+from util.code import try_parse
+
+translate = QCoreApplication.translate
+
+class AlgoCommentStmt(QDialog):
+    def __init__(self, parent, origcode=""):
+        super().__init__(parent)
+        self.ui = Ui_AlgoCommentStmt()
+        self.ui.setupUi(self)
+        self.setFixedSize(self.size())
+        self.ui.lineEdit.setText(origcode)
+        center_widget(self, parent)
+
+
+    def done(self, res):
+        if res == QDialog.Accepted:
+            self.comment = self.ui.lineEdit.text()
+            self.ok = True
+
+        super(AlgoCommentStmt, self).done(res)
+
+
+    def run(self):
+        return self.exec_() == QDialog.Accepted and self.ok
