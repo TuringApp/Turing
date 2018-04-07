6c09873a9bf624c7046e400a89ca91b3412d6bd9
zdimension <zippedfire@free.fr>
2018-04-07 22:07:05 +0200

    add algobox lib

--- src/maths/lib/algobox.py
+++ src/maths/lib/algobox.py
@@ -1,23 +1,30 @@
 # -*- coding: utf-8 -*-
-
-
+from maths.lib.stats import *
+from maths.lib.basic import *
 from util import translate
 from .docs import *
 
 __desc__ = translate("Docs", "Algobox compatibility")
 
-doc("round",
-    [
-        ("num", "Number"),
-        ("prec", "Integer", None, None)
-    ],
-    translate("Docs", "Rounds {{num}} to the nearest integer / (if specified) to {{prec}} decimals."),
-    ["arrondi"])
-
-
-def round(num, prec=None):
-    if type(num) == complex:
-        return complex(round(num.real, prec), round(num.imag, prec))
-    if prec:
-        return builtins.round(num, int(prec))
-    return builtins.round(num)
+ALGOBOX_ALEA_ENT = randint
+ALGOBOX_COEFF_BINOMIAL = binomial
+ALGOBOX_LOI_BINOMIAL = None
+ALGOBOX_LOI_NORMALE_CR = None
+ALGOBOX_LOI_NORMALE = None
+ALGOBOX_INVERSE_LOI_NORMALE_CR = None
+ALGOBOX_INVERSE_LOI_NORMALE = None
+ALGOBOX_FACTORIELLE = fact
+ALGOBOX_SOMME = lambda lst, p, n: sum(lst[p:n+1])
+ALGOBOX_MOYENNE = lambda lst, p, n: arithm_mean(lst[p:n+1])
+ALGOBOX_VARIANCE = lambda lst, p, n: variance(lst[p:n+1])
+ALGOBOX_ECART_TYPE = lambda lst, p, n: stand_dev(lst[p:n+1])
+ALGOBOX_MEDIANE = None
+ALGOBOX_QUARTILE1 = None
+ALGOBOX_QUARTILE3 = None
+ALGOBOX_QUARTILE1_BIS = None
+ALGOBOX_QUARTILE3_BIS = None
+ALGOBOX_MINIMUM = lambda lst, p, n: min(lst[p:n+1])
+ALGOBOX_MAXIMUM = lambda lst, p, n: max(lst[p:n+1])
+ALGOBOX_POS_MINIMUM = lambda lst, p, n: lst[p:n+1].index(ALGOBOX_MINIMUM(lst[p:n+1]))
+ALGOBOX_POS_MAXIMUM = lambda lst, p, n: lst[p:n+1].index(ALGOBOX_MAXIMUM(lst[p:n+1]))
+ALGOBOX_ARRONDIR = round
