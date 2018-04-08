ba83688246288acf3b6129fa961bb2f293aa2a09
zdimension <zippedfire@free.fr>
2018-04-08 16:23:40 +0200

    fix assign display

--- src/main.py
+++ src/main.py
@@ -1092,7 +1092,7 @@ def str_stmt(stmt):
         if stmt.value is None:
             ret = translate("Algo", "[k]DECLARE[/k] [c]{var}[/c]").format(var=stmt.variable)
         else:
-            ret = translate("Algo", "[k]ASSIGN[/k] [c]{var}[/c] = [c]{value}[/c]").format(var=code(stmt.variable),
+            ret = translate("Algo", "[k]VARIABLE[/k] [c]{var}[/c] TAKES VALUE [c]{value}[/c]").format(var=code(stmt.variable),
                                                                                           value=code(stmt.value))
 
     elif isinstance(stmt, CallStmt):
