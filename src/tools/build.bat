b453d821496da06332b0a6dccc9a48d1e6d65ae3
zdimension <zippedfire@free.fr>
2018-04-10 12:37:41 +0200

    fix build script

--- src/tools/build.bat
+++ src/tools/build.bat
@@ -1,3 +1,3 @@
 @echo off
-pyinstaller --onefile --additional-hooks-dir=. --icon=media/icon.ico -y main.py
+pyinstaller --onefile --additional-hooks-dir=. --icon=media/icon.ico --windowed -n turing -y main.py
 pyinstaller --onefile --additional-hooks-dir=. --icon=media/icon.ico --hidden-import pyqode.python.backend -y editor_backend.py
