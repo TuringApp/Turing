20334d78ad2f96402c910ae9cbc293313070262e
Georges Khaznadar <georgesk@debian.org>
2018-05-15 19:30:07 +0200

    imported a frozen version of pyqode since it is no longer maintained

---
+++ pyqode/python/widgets/outline.py
@@ -0,0 +1,13 @@
+"""
+This document contains the tree widget used to display the editor document
+outline.
+
+"""
+from pyqode.core.widgets import OutlineTreeWidget
+
+
+class PyOutlineTreeWidget(OutlineTreeWidget):
+    """
+    Deprecated, use pyqode.core.widgets.OutlineTreeWidget instead.
+    """
+    pass
